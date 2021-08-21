50e6860e4fc88f9426e2f1c302a99d335b9a8d03
Kaspok <kaspok251198u@yandex.ru>
2021-08-21 23:16:34 +0300

    Добавил API тесты на запросы методом POST

--- src/test/java/in/reqres/tests/ApiTests.java
+++ src/test/java/in/reqres/tests/ApiTests.java
@@ -1,8 +1,11 @@
 package in.reqres.tests;
 
+import com.github.javafaker.Faker;
 import in.reqres.models.User;
 import in.reqres.models.UserData;
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.json.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -11,9 +14,16 @@ import java.util.Optional;
 import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
 
 
 public class ApiTests {
+
+    Faker faker = new Faker();
+
+    private String name = faker.name().firstName();
+    private String job = faker.job().position();
+    private String email = faker.internet().emailAddress();
 
     @BeforeEach
     void beforeEach() {
@@ -40,10 +50,49 @@ public class ApiTests {
                 .then()
                 .statusCode(200)
                 .extract().response().as(UserData.class);
+
         Optional<User> first = listUser
                 .getUsers()
                 .stream()
                 .filter(user -> user.getId() == 11).findFirst();
+
         assertThat(first.get().getFullName()).isEqualTo("George Edwards");
+    }
+
+
+    @Test
+    void createUserTest() {
+        JSONObject userData = new JSONObject()
+                .put("name", this.name)
+                .put("job", this.job);
+
+        given()
+                .contentType(ContentType.JSON)
+                .body(userData.toString())
+                .when()
+                .post("/users")
+                .then()
+                .statusCode(201)
+                .body("name", equalTo(name))
+                .body("job", equalTo(job))
+                .body("id", notNullValue());
+    }
+
+    @Test
+    void negativeLoginTest() {
+        JSONObject email = new JSONObject()
+                .put("email", this.email);
+
+        String response = given()
+                .contentType(ContentType.JSON)
+                .body(email.toString())
+                .when()
+                .post("/register")
+                .then()
+                .statusCode(400)
+                .extract().response().jsonPath().getString("error");
+
+        assertThat(response).isEqualTo("Missing password");
+
     }
 }
